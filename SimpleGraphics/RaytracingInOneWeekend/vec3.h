a53f909a3d5c06334a6049f4c38d6f32680c9263
Chiaki-meow <chiaki_meow@sjtu.edu.cn>
2025-03-24 01:59:34 +0800

    update: 完成基本的漫反射

--- SimpleGraphics/RaytracingInOneWeekend/vec3.h
+++ SimpleGraphics/RaytracingInOneWeekend/vec3.h
@@ -4,6 +4,9 @@
 #include <cmath>
 #include <iostream>
 
+extern inline double random_double();
+extern inline double random_double(double min, double max);
+
 class vec3 {
 public:
   double e[3];
@@ -39,6 +42,15 @@ public:
   }
 
   double length() const { return std::sqrt(length_squared()); }
+
+  static vec3 random() {
+    return vec3(random_double(), random_double(), random_double());
+  }
+
+  static vec3 random(double min, double max) {
+    return vec3(random_double(min, max), random_double(min, max),
+                random_double(min, max));
+  }
 };
 
 using point3 = vec3;
@@ -63,9 +75,7 @@ inline vec3 operator*(double t, const vec3 &v) {
   return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
 }
 
-inline vec3 operator*(const vec3 &v, double t) {
-  return t*v;
-}
+inline vec3 operator*(const vec3 &v, double t) { return t * v; }
 
 inline vec3 operator/(const vec3 &v, double t) { return (1 / t) * v; }
 
@@ -76,12 +86,26 @@ inline double dot(const vec3 &u, const vec3 &v) {
 inline vec3 cross(const vec3 &u, const vec3 &v) {
   return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
               u.e[2] * v.e[0] - u.e[0] * v.e[2],
-              u.e[0] * v.e[1] - u.e[1] * v.e[0]
-              );
+              u.e[0] * v.e[1] - u.e[1] * v.e[0]);
 }
 
-inline vec3 unit_vector(const vec3 &v) {
-  return v/v.length();
+inline vec3 unit_vector(const vec3 &v) { return v / v.length(); }
+
+inline vec3 random_unit_vector() {
+  while (true) {
+    auto p = vec3::random(-1, 1);
+    auto lensq = p.length_squared();
+    if (1e-160 < lensq && lensq <= 1)
+      return p / sqrt(lensq);
+  }
+}
+
+inline vec3 random_on_hemisphere(const vec3 &normal) {
+  vec3 on_unit_sphere = random_unit_vector();
+  if (dot(on_unit_sphere, normal) > 0) // 和法线在同一个半球
+    return on_unit_sphere;
+  else
+   return -on_unit_sphere;
 }
 
 #endif // VEC3_H
