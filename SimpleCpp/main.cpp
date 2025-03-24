be9a38d821788cb506e1d0297ba3cc4477150e54
Chiaki-meow <chiaki_meow@sjtu.edu.cn>
2025-03-24 20:26:38 +0800

    update: 测试了enable_shared_from_this

--- SimpleCpp/main.cpp
+++ SimpleCpp/main.cpp
@@ -11,12 +11,14 @@ void vector_int_test();
 void vector_test_class_test();
 void shared_ptr_test_class_test();
 void initializer_list_test();
+void enable_shared_from_this_test();
 
 int main() {
-  vector_int_test();
-  vector_test_class_test();
-  shared_ptr_test_class_test();
-  initializer_list_test();
+  // vector_int_test();
+  // vector_test_class_test();
+  // shared_ptr_test_class_test();
+  // initializer_list_test();
+  enable_shared_from_this_test();
   return 0;
 }
 
@@ -81,8 +83,12 @@ void shared_ptr_test_class_test() {
   ptr3->getValue();
 }
 
-class Widget {
+
+class Widget : public std::enable_shared_from_this<Widget> {
 public:
+  Widget() { std::cout << "Default constructor" << std::endl; };
+
+  // 测试initializer_list的用法
   Widget(int i, bool b) { std::cout << "Constructor (int, bool)" << std::endl; }
   Widget(int i, double d) {
     std::cout << "Constructor (int, double)" << std::endl;
@@ -91,13 +97,24 @@ public:
     std::cout << "Constructor (initializer_list<long double>)" << std::endl;
   }
   Widget(const Widget &other) { std::cout << "Constructor Copy" << std::endl; }
-  // 有内部的string类型转换：会调用拷贝构造函数
+  // 有内部的string类型转换：会调用拷贝构造函数，不会用string重载
   // operator std::string() const { std::cout<< "Constructor String" <<
   // std::endl; return "Widget_String"; }
+
+  // 有内部的float类型转换
   operator float() const {
     std::cout << "Constructor Float" << std::endl;
     return 0.0;
   }
+
+  // 测试 enable_shared_from_this 的作用
+  std::shared_ptr<Widget> getSharedPtr() {
+    // return std::shared_ptr<Widget>(this);  // 错误❌ 会使得返回一个和本体的shared_ptr相独立的shared_ptr
+    return shared_from_this();  // ✅ 正确获取已有 shared_ptr
+  }
+
+  
+  ~Widget() { std::cout << "Widget destroyed\n"; }
 };
 
 void initializer_list_test() {
@@ -107,4 +124,12 @@ void initializer_list_test() {
 
   Widget w4 = w1; // OK: 调用拷贝构造（隐式）
   Widget w6{w4};
+}
+
+void enable_shared_from_this_test() {
+  Widget widget;
+  std::shared_ptr<Widget> p1 = std::make_shared<Widget>();
+  std::shared_ptr<Widget> p2 = p1->getSharedPtr();
+
+  std::cout << "Use count: " << p1.use_count() << std::endl;
 }
