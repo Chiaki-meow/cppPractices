47e610d200c0260679a8618014252ea5407f4fd8
Chiaki-meow <chiaki_meow@sjtu.edu.cn>
2025-03-28 20:00:55 +0800

    update: 完成折射

--- SimpleGraphics/RaytracingInOneWeekend/material.h
+++ SimpleGraphics/RaytracingInOneWeekend/material.h
@@ -53,7 +53,7 @@ private:
 
 class dielectric : public material {
 public:
-  dielectric(double refraction_index) : refraction_index(refraction_index){}
+  dielectric(double refraction_index) : refraction_index(refraction_index) {}
 
   bool scatter(const ray &r_in, const hit_record &rec, color &attenuation,
                ray &scattered) const override {
@@ -64,21 +64,28 @@ public:
     double cos_theta = std::fmin(dot(-unit_direction, rec.normal), 1.0);
     double sin_theta = std::sqrt(1.0 - cos_theta * cos_theta);
 
-    bool cannot_refract = ri * sin_theta;
+    bool cannot_refract = ri * sin_theta > 1.0;
     vec3 direction;
 
-    if (cannot_refract) {
+    if (cannot_refract || reflectance(cos_theta, ri) > random_double()) {
       direction = reflect(unit_direction, rec.normal);
     } else {
       direction = refract(unit_direction, rec.normal, ri);
-      }
-    
+    }
+
     scattered = ray(rec.p, direction);
     return true;
   }
 
 private:
   double refraction_index;
+
+  static double reflectance(double cosine, double refraction_index) {
+    // 使用Schlick Approximation进行玻璃根据角度变化的反射率近似
+    auto r0 = (1 - refraction_index) / (1 + refraction_index);
+    r0 *= r0;
+    return r0 + (1 - r0) * std::pow((1 - cosine), 5);
+  }
 };
 
 #endif // MATERIAL_H
